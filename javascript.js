62330a410cd800171691b22277a8f34b0f160329
Anirvesh Mangipudi <aniristhebest@gmail.com>
2024-12-28 08:48:02 +0100

    Added a change grid size button

--- javascript.js
+++ javascript.js
@@ -1,25 +1,64 @@
-let i = 0, j = 0;
+function createGrid(col, row) {
 
-let cont = document.querySelector(".container");
+    let grid = document.createElement("div");
 
-for (i=0;i<16;i++)
-{
-    let column_item = document.createElement("div");
+    grid.classList.add("grid");
 
-    column_item.classList.add("column-item");
+    let i = 0, j = 0;
 
-    for (j=0;j<16;j++)
+    let cont = document.querySelector(".container");
+
+    cont.appendChild(grid);
+
+    for (i=0;i<col;i++)
     {
-        let row_item = document.createElement("div");
+        let column_item = document.createElement("div");
 
-        row_item.classList.add("row-item");
+        column_item.classList.add("column-item");
 
-        row_item.addEventListener("mouseover", (event) => {
-            row_item.style.backgroundColor = "black";
-        })
+        for (j=0;j<row;j++)
+        {
+            let row_item = document.createElement("div");
 
-        column_item.appendChild(row_item);
+            row_item.classList.add("row-item");
+
+            row_item.style.opacity = 0.1;
+
+            row_item.addEventListener("mouseover", (event) => {
+                row_item.style.backgroundColor = "black";
+
+                let x = parseFloat(row_item.style.opacity);
+
+                if (x<1) row_item.style.opacity = x + 0.2;
+            })
+
+            column_item.appendChild(row_item);
+        }
+
+        grid.appendChild(column_item);
     }
-
-    cont.appendChild(column_item);
 }
+
+function deleteGrid() {
+    let cont = document.querySelector(".container");
+
+    let grid = document.querySelector(".grid");
+
+    cont.removeChild(grid);
+}
+
+let button = document.querySelector("#input");
+
+button.addEventListener("click", () => {
+    let gridSize = prompt("Enter a grid size between 15 and 100: ");
+
+    if(gridSize<15 || gridSize>100 || !Number.isInteger(Number(gridSize))) {
+        alert("Invalid input");
+        return;}
+
+    deleteGrid();
+
+    createGrid(Number(gridSize), Number(gridSize));
+})
+
+createGrid(16,16);
